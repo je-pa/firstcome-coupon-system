1f96d1d05765b34a893bc649082a4002bcad60ba
je-pa <jieun0502@gmail.com>
2024-05-30 03:42:46 +0900

    ✨ Features: 간단 선착순
    Signed-off-by: je-pa <jieun0502@gmail.com>

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -1,8 +1,14 @@
 package com.example.api.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.example.api.repository.CouponRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
