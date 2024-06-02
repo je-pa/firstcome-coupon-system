b84dae72bf3e6ab3ce2a590a10c904f8376255af
je-pa <jieun0502@gmail.com>
2024-06-03 05:39:33 +0900

    ✨ Features: Set를 활용한 선착순 로직 1인 1쿠폰 제한
    Signed-off-by: je-pa <jieun0502@gmail.com>

---
+++ api/src/main/java/com/example/api/service/ApplyWithSetService.java
@@ -0,0 +1,31 @@
+package com.example.api.service;
+
+import com.example.api.producer.CouponCreateProducer;
+import com.example.api.repository.AppliedUserRepository;
+import com.example.api.repository.CouponCountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ApplyWithSetService {
+  private final CouponCountRepository couponCountRepository;
+  private final CouponCreateProducer couponCreateProducer;
+  private final AppliedUserRepository appliedUserRepository;
+
+  public void apply(Long userId) {
+    Long apply = appliedUserRepository.add(userId);
+
+    if(apply != 1){
+      return;
+    }
+
+    Long count = couponCountRepository.increment();
+
+    if(count > 100){
+      return;
+    }
+
+    couponCreateProducer.create(userId);
+  }
+}
